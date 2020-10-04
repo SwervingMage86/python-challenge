9d247f706af9839cc0a40304534ec0b74b62bddd
Tony Odom <todom86@gmail.com>
2020-10-04 17:29:57 -0600

    found average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-path = os.path.join('Resources', 'budget_data.csv')
+path = os.path.join('PyBank','Resources', 'budget_data.csv')
 change = []
 net = 0
 
@@ -16,12 +16,33 @@ with open(path) as budget_file:
     reader = csv.reader(budget_file, delimiter=",")
     header = next(reader)
 
-    profit_loss = [int(profit[1]) for profit in reader]
-            
+    monthly_profit = [int(profit[1]) for profit in reader]
+
+    first_month = monthly_profit[0]
+    previous_month = 0
     
-            
+    for i in monthly_profit:
+        if i == monthly_profit[0]:
+            first_month = monthly_profit[0]
+            previous_month = i
+        else:
+            change.append(i-previous_month)
+            previous_month = i
+    
+    average_change = sum(change) / (count-1)
+
+   
 
     
+        
+        
+
+        
+        
+
 
 print("Total Months:" + " " + str(count))
-print("Total:" + " " + "$" + str(sum(profit_loss)))
+print("Total:" + " " + "$" + str(sum(monthly_profit)))
+print("Average Change" + " " + "$" + str(round(average_change, 2)))
+
+
