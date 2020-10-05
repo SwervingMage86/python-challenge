8d512dfbf9facce67177cb3683fb56c53c1fd763
Tony Odom <todom86@gmail.com>
2020-10-04 22:15:34 -0600

    added notes to code and ensure everything operates correctly

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,44 +1,63 @@
+#Import Modules
 import os
 import csv
 
+#Declare file path
 path = os.path.join('PyBank','Resources', 'budget_data.csv')
 
+#Define needed lists
 monthly_profit = []
 month_list = []
 change = []
 
+#Read file
 with open(path) as budget_file:
-
     reader = csv.reader(budget_file, delimiter=",")
+
+    #Skip header
     header = next(reader)
+
+    #Count number of rows for total month count    
     count = sum(1 for row in reader)
 
+#Read file...again
 with open(path) as budget_file:
-
     reader = csv.reader(budget_file, delimiter=",")
+
+    #Skip header...again
     header = next(reader)
 
+    #Loop through rows to seperate data into two different lists
     for row in reader:
         monthly_profit.append(int(row[1]))
         month_list.append(str(row[0]))
     
-
+    #Determine value of the first month's profit/loss
     first_month = monthly_profit[0]
+
+    #Define variable for use later
     previous_month = 0
     
+    #Loop through monthly profit list to determine month to month change
     for i in monthly_profit:
         if i == monthly_profit[0]:
             first_month = monthly_profit[0]
             previous_month = i
+
+        #Append monthly change values to seperate list
         else:
             change.append(i-previous_month)
             previous_month = i
-    
+
+        #Determine average change
         average_change = sum(change) / (count-1)
-    
+
+    #Find the index values of largest increase and decrease in monthly change
     max_index = change.index(max(change))
     min_index = change.index(min(change))
 
+    #Match Largest increase and decrease to date(add +1 because there is no first month change)
+    #Assigned value as string to a variable
     max_month = str(month_list[max_index + 1])
     max_change = ("$" + str(max(change)))
 
@@ -46,7 +65,7 @@ with open(path) as budget_file:
     min_change = ("$" + str(min(change)))
 
 
-
+#Print outcomes in terminal
 print("Financial Analysis")
 print("------------------------")
 print("Total Months:" + " " + str(count))
@@ -55,6 +74,7 @@ print("Average Change" + " " + "$" + str(round(average_change, 2)))
 print("Greatest Increase in Profit:" + " " + max_month + " " + "(" + max_change + ")")
 print("Greatest Decrease in Profit:" + " " + min_month + " " + "(" + min_change + ")")
 
+#Output values to a text file
 output = os.path.join("Pybank","Analysis","budget_analysis.txt")
 
 with open(output, 'w') as txt_file:
@@ -67,6 +87,3 @@ with open(output, 'w') as txt_file:
     txt_file.write("Greatest Increase in Profit:" + " " + max_month + " " + "(" + max_change + ")" + os.linesep)
     txt_file.write("Greatest Decrease in Profit:" + " " + min_month + " " + "(" + min_change + ")" + os.linesep)
     
-    
-
-
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,24 +1,34 @@
+#Import modules
 import os
 import csv
 
+#Declare Path
 path = os.path.join("PyPoll","Resources","election_data.csv")
 
+#Define needed lists
 Khan = []
 Tooley = []
 Li = []
 Correy = []
 
+#Read File
 with open(path) as election_file:
-
     reader = csv.reader(election_file)
+
+    #Skip Header
     header = next(reader)
+
+    #Count number of rows for total votes
     count = sum(1 for votes in reader)
 
+#Read File...again
 with open(path) as election_file:
-
     reader = csv.reader(election_file, delimiter=",")
+
+    #Skip header...again
     header = next(reader)
 
+    #Loop through file for individual vote count and append to seperate lists
     for votes in reader:
 
         if votes[2] == "O'Tooley":
@@ -30,6 +40,7 @@ with open(path) as election_file:
         else:
             Correy.append(votes)
 
+#Tally votes for each candidate and find percentage
 tooley_count = sum(1 for tvote in Tooley)
 tooley_percent = tooley_count / count * 100
 
@@ -42,23 +53,28 @@ khan_percent = khan_count / count * 100
 correy_count = sum(1 for cvote in Correy)
 correy_percent = correy_count / count * 100
 
+#Create lists for candidate vote count and candidates
+#Ensure indexes for both match their respective indexes
 winner_list = [tooley_count, li_count, khan_count, correy_count]
 candidate_list = ["O'Tooley", "Li", "Khan", "Correy"]
 
+#Determine highest vote count
 winner_index = winner_list.index(max(winner_list))
 winner = candidate_list[winner_index]
 
+#Output results in terminal
 print("Election Results")
 print("------------------------")
 print("Total Votes: " + str(count))
 print("------------------------")
-print("O'Tooley: " + str(round(tooley_percent, 2)) + "%" + " " + "(" + str(tooley_count) + ")") 
-print("Li: " + str(round(li_percent, 2)) + "%" + " " + "(" + str(li_count) + ")")
-print("Khan: " + str(round(khan_percent, 2)) + "%" + " " + "(" + str(khan_count) + ")")
-print("Correy: " + str(round(correy_percent, 2)) + "%" + " " + "(" + str(correy_count) + ")")
+print("O'Tooley: " + str(round(tooley_percent, 2)) + "%" + " " + "(" + str(tooley_count) + " votes)") 
+print("Li: " + str(round(li_percent, 2)) + "%" + " " + "(" + str(li_count) + " votes)")
+print("Khan: " + str(round(khan_percent, 2)) + "%" + " " + "(" + str(khan_count) + " votes)")
+print("Correy: " + str(round(correy_percent, 2)) + "%" + " " + "(" + str(correy_count) + " votes)")
 print("------------------------")
 print("Winner: " + winner)
 
+#Output results in seperate text file
 output = os.path.join("PyPoll","Analysis","election_results.txt")
 
 with open(output, 'w') as txt_file:
@@ -67,9 +83,9 @@ with open(output, 'w') as txt_file:
     txt_file.write("------------------------" + os.linesep)
     txt_file.write("Total Votes: " + str(count) + os.linesep)
     txt_file.write("------------------------" + os.linesep)
-    txt_file.write("O'Tooley: " + str(round(tooley_percent, 2)) + "%" + " " + "(" + str(tooley_count) + ")" + os.linesep) 
-    txt_file.write("Li: " + str(round(li_percent, 2)) + "%" + " " + "(" + str(li_count) + ")" + os.linesep)
-    txt_file.write("Khan: " + str(round(khan_percent, 2)) + "%" + " " + "(" + str(khan_count) + ")" + os.linesep)
-    txt_file.write("Correy: " + str(round(correy_percent, 2)) + "%" + " " + "(" + str(correy_count) + ")" + os.linesep)
+    txt_file.write("O'Tooley: " + str(round(tooley_percent, 2)) + "%" + " " + "(" + str(tooley_count) + " votes)" + os.linesep)
+    txt_file.write("Li: " + str(round(li_percent, 2)) + "%" + " " + "(" + str(li_count) + " votes)" + os.linesep)
+    txt_file.write("Khan: " + str(round(khan_percent, 2)) + "%" + " " + "(" + str(khan_count) + " votes)" + os.linesep)
+    txt_file.write("Correy: " + str(round(correy_percent, 2)) + "%" + " " + "(" + str(correy_count) + " votes)" + os.linesep)
     txt_file.write("------------------------" + os.linesep)
     txt_file.write("Winner: " + winner + os.linesep)
