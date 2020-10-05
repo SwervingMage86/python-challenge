a679054fc083fe1e436d003b77de629fb1fa05c1
Tony Odom <todom86@gmail.com>
2020-10-04 21:45:30 -0600

    cleaned up pypoll code and added output file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,8 +14,6 @@ with open(path) as election_file:
     header = next(reader)
     count = sum(1 for votes in reader)
 
-print("Total Votes: " + str(count))
-
 with open(path) as election_file:
 
     reader = csv.reader(election_file, delimiter=",")
@@ -34,23 +32,44 @@ with open(path) as election_file:
 
 tooley_count = sum(1 for tvote in Tooley)
 tooley_percent = tooley_count / count * 100
-print("O'Tooley: " + str(round(tooley_percent, 2)) + "%" + " " + "(" + str(tooley_count) + ")") 
 
 li_count = sum(1 for lvote in Li)
 li_percent = li_count / count * 100
-print("Li: " + str(round(li_percent, 2)) + "%" + " " + "(" + str(li_count) + ")")
 
 khan_count = sum(1 for kvote in Khan)
 khan_percent = khan_count / count * 100
-print("Khan: " + str(round(khan_percent, 2)) + "%" + " " + "(" + str(khan_count) + ")")
 
 correy_count = sum(1 for cvote in Correy)
 correy_percent = correy_count / count * 100
-print("Correy: " + str(round(correy_percent, 2)) + "%" + " " + "(" + str(correy_count) + ")")
 
 winner_list = [tooley_count, li_count, khan_count, correy_count]
 candidate_list = ["O'Tooley", "Li", "Khan", "Correy"]
 
 winner_index = winner_list.index(max(winner_list))
 winner = candidate_list[winner_index]
+
+print("Election Results")
+print("------------------------")
+print("Total Votes: " + str(count))
+print("------------------------")
+print("O'Tooley: " + str(round(tooley_percent, 2)) + "%" + " " + "(" + str(tooley_count) + ")") 
+print("Li: " + str(round(li_percent, 2)) + "%" + " " + "(" + str(li_count) + ")")
+print("Khan: " + str(round(khan_percent, 2)) + "%" + " " + "(" + str(khan_count) + ")")
+print("Correy: " + str(round(correy_percent, 2)) + "%" + " " + "(" + str(correy_count) + ")")
+print("------------------------")
 print("Winner: " + winner)
+
+output = os.path.join("PyPoll","Analysis","election_results.txt")
+
+with open(output, 'w') as txt_file:
+     
+    txt_file.write("Election Results" + os.linesep)
+    txt_file.write("------------------------" + os.linesep)
+    txt_file.write("Total Votes: " + str(count) + os.linesep)
+    txt_file.write("------------------------" + os.linesep)
+    txt_file.write("O'Tooley: " + str(round(tooley_percent, 2)) + "%" + " " + "(" + str(tooley_count) + ")" + os.linesep) 
+    txt_file.write("Li: " + str(round(li_percent, 2)) + "%" + " " + "(" + str(li_count) + ")" + os.linesep)
+    txt_file.write("Khan: " + str(round(khan_percent, 2)) + "%" + " " + "(" + str(khan_count) + ")" + os.linesep)
+    txt_file.write("Correy: " + str(round(correy_percent, 2)) + "%" + " " + "(" + str(correy_count) + ")" + os.linesep)
+    txt_file.write("------------------------" + os.linesep)
+    txt_file.write("Winner: " + winner + os.linesep)
